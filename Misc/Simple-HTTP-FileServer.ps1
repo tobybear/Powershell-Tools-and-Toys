1897d463db5eefdbfc095ba8b3031354498140d4
beigeworm <93350544+beigeworm@users.noreply.github.com>
2023-07-28 11:12:54 +0000

    Update Simple-HTTP-FileServer.ps1

--- Misc/Simple-HTTP-FileServer.ps1
+++ Misc/Simple-HTTP-FileServer.ps1
@@ -20,14 +20,6 @@ Run script and input given URL in a browser.
 #>
 
 #============================================================ OPEN MESSAGE ====================================================================
-
-Write-Host "=======================================================================================" -ForegroundColor Green -BackgroundColor Black
-Write-Host "██╗  ██╗████████╗████████╗██████╗     ███████╗███████╗██████╗ ██╗   ██╗███████╗██████╗ " -ForegroundColor Green -BackgroundColor Black
-Write-Host "██║  ██║╚══██╔══╝╚══██╔══╝██╔══██╗    ██╔════╝██╔════╝██╔══██╗██║   ██║██╔════╝██╔══██╗" -ForegroundColor Green -BackgroundColor Black
-Write-Host "███████║   ██║      ██║   ██████╔╝    ███████╗█████╗  ██████╔╝██║   ██║█████╗  ██████╔╝" -ForegroundColor Green -BackgroundColor Black
-Write-Host "██╔══██║   ██║      ██║   ██╔═══╝     ╚════██║██╔══╝  ██╔══██╗╚██╗ ██╔╝██╔══╝  ██╔══██╗" -ForegroundColor Green -BackgroundColor Black
-Write-Host "██║  ██║   ██║      ██║   ██║         ███████║███████╗██║  ██║ ╚████╔╝ ███████╗██║  ██║" -ForegroundColor Green -BackgroundColor Black
-Write-Host "╚═╝  ╚═╝   ╚═╝      ╚═╝   ╚═╝         ╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝" -ForegroundColor Green -BackgroundColor Black
 Write-Host "=======================================================================================" -ForegroundColor Green -BackgroundColor Black
 Write-Host "============================= Simple HTTP File Server =================================" -ForegroundColor Green -BackgroundColor Black
 Write-Host "=======================================================================================`n" -ForegroundColor Green -BackgroundColor Black

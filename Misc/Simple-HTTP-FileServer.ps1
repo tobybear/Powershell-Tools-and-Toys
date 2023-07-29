7a721a42ec6ff661b2fb56a5f2ccf767baf608f6
beigeworm <93350544+beigeworm@users.noreply.github.com>
2023-07-29 12:23:23 +0000

    Update Simple-HTTP-FileServer.ps1

--- Misc/Simple-HTTP-FileServer.ps1
+++ Misc/Simple-HTTP-FileServer.ps1
@@ -25,6 +25,8 @@ Clear-Host
 $width = 88
 $height = 30
 [Console]::SetWindowSize($width, $height)
+$windowTitle = "HTTP File Server"
+[Console]::SetTitle($windowTitle)
 Write-Host "=======================================================================================" -ForegroundColor Green -BackgroundColor Black
 Write-Host "============================= Simple HTTP File Server =================================" -ForegroundColor Green -BackgroundColor Black
 Write-Host "=======================================================================================`n" -ForegroundColor Green -BackgroundColor Black

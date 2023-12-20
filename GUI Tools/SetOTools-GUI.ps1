ca940d3c93b629935472a77c426b19f2e8b51648
egieb <93350544+beigeworm@users.noreply.github.com>
2023-12-20 14:01:07 +0000

    Update SetOTools-GUI.ps1

--- GUI Tools/SetOTools-GUI.ps1
+++ GUI Tools/SetOTools-GUI.ps1
@@ -27,13 +27,22 @@ if ($NCurl.Length -eq 0){$NCurl = "192.168.0.1"} # Change this to open GUI with 
 
 $hidewindow = 1
 If ($HideWindow -gt 0){
-$Async = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
-$Type = Add-Type -Member $Async -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
-$hwnd = (Get-Process -PID $pid).MainWindowHandle
-$Host.UI.RawUI.WindowTitle = 'hideme'
-$Proc = (Get-Process | Where-Object {$_.MainWindowTitle -eq 'hideme'})
-$hwnd = $Proc.MainWindowHandle
-$Type::ShowWindowAsync($hwnd, 0)
+    If ($PSScriptRoot.length -eq 0){
+        write-host "Script NOT Local"
+        $Import = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);';
+        add-type -name win -member $Import -namespace native;
+        [native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0);
+    }
+    else{
+        write-host "Script IS Local"
+        $Async = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
+        $Type = Add-Type -Member $Async -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
+        $hwnd = (Get-Process -PID $pid).MainWindowHandle
+        $Host.UI.RawUI.WindowTitle = 'hideme'
+        $Proc = (Get-Process | Where-Object {$_.MainWindowTitle -eq 'hideme'})
+        $hwnd = $Proc.MainWindowHandle
+        $Type::ShowWindowAsync($hwnd, 0)
+    }
 }
 
 $imageUrl = "https://i.ibb.co/ZGrt8qb/b-min.png"

255d5a08c93fa9b6194f83729c705a0a06ed84c9
egieb <93350544+beigeworm@users.noreply.github.com>
2023-12-20 14:13:53 +0000

    Update Hide-Windows-Terminal.ps1

--- Misc/Hide-Windows-Terminal.ps1
+++ Misc/Hide-Windows-Terminal.ps1
@@ -5,9 +5,15 @@ This script hides any powershell OR terminal window.
 #>
 
 $Async = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
-$Type = Add-Type -Member $Async -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
+$Type = Add-Type -MemberDefinition $Async -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
 $hwnd = (Get-Process -PID $pid).MainWindowHandle
-$Host.UI.RawUI.WindowTitle = 'hideme'
-$Proc = (Get-Process | Where-Object {$_.MainWindowTitle -eq 'hideme'})
-$hwnd = $Proc.MainWindowHandle
-$Type::ShowWindowAsync($hwnd, 0)
+if($hwnd -ne [System.IntPtr]::Zero){
+    $Type::ShowWindowAsync($hwnd, 0)
+}
+else{
+    $Host.UI.RawUI.WindowTitle = 'hideme'
+    $Proc = (Get-Process | Where-Object { $_.MainWindowTitle -eq 'hideme' })
+    $hwnd = $Proc.MainWindowHandle
+    $Type::ShowWindowAsync($hwnd, 0)
+}
+

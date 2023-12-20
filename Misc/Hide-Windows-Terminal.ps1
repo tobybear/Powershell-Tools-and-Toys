cdd5015d62c26df312699f752304ab2fd0e4ee32
egieb <93350544+beigeworm@users.noreply.github.com>
2023-12-20 13:07:31 +0000

    Update Hide-Windows-Terminal.ps1

--- Misc/Hide-Windows-Terminal.ps1
+++ Misc/Hide-Windows-Terminal.ps1
@@ -1,11 +1,13 @@
+<# =============== Hide Windows 11 Terminal Application =====================
+
+Windows 11 22H2 and onwards, powershell opens with windows terminal by default.
+This script hides any powershell OR terminal window.
+#>
+
 $Async = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
-$Type = Add-Type -MemberDefinition $Async -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
+$Type = Add-Type -Member $Async -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
 $hwnd = (Get-Process -PID $pid).MainWindowHandle
 $Host.UI.RawUI.WindowTitle = 'hideme'
-$Proc = (Get-Process | Where-Object { $_.MainWindowTitle -eq 'hideme' })
+$Proc = (Get-Process | Where-Object {$_.MainWindowTitle -eq 'hideme'})
 $hwnd = $Proc.MainWindowHandle
 $Type::ShowWindowAsync($hwnd, 0)
-
-sleep 2
-New-Item -ItemType File -Path "$env:USERPROFILE/Desktop/RUNS_HIDDEN.txt"
-exit

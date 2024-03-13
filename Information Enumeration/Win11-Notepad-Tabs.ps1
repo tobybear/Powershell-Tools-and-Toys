82c5576732edd0a1e85aa612a30b557580752eae
egieb <93350544+beigeworm@users.noreply.github.com>
2024-03-13 16:06:17 +0000

    Update Win11-Notepad-Tabs.ps1

--- Information Enumeration/Win11-Notepad-Tabs.ps1
+++ Information Enumeration/Win11-Notepad-Tabs.ps1
@@ -1,9 +1,7 @@
 $outpath = "notepad.txt"
-
 $appDataDir = [Environment]::GetFolderPath('LocalApplicationData')
 $directoryRelative = "Packages\Microsoft.WindowsNotepad_*\LocalState\TabState"
 $matchingDirectories = Get-ChildItem -Path (Join-Path -Path $appDataDir -ChildPath 'Packages') -Filter 'Microsoft.WindowsNotepad_*' -Directory
-
 foreach ($dir in $matchingDirectories) {
     $fullPath = Join-Path -Path $dir.FullName -ChildPath 'LocalState\TabState'
     $listOfBinFiles = Get-ChildItem -Path $fullPath -Filter *.bin
@@ -11,14 +9,12 @@ foreach ($dir in $matchingDirectories) {
         if ($fullFilePath.Name -like '*.0.bin' -or $fullFilePath.Name -like '*.1.bin') {
             continue
         }
-
         $seperator = ("=" * 60)
         $SMseperator = ("-" * 60)
         $seperator | Out-File -FilePath $outpath -Append
         $filename = $fullFilePath.Name
         $contents = [System.IO.File]::ReadAllBytes($fullFilePath.FullName)
         $isSavedFile = $contents[3]
-
         if ($isSavedFile -eq 1) {
             $lengthOfFilename = $contents[4]
             $filenameEnding = 5 + $lengthOfFilename * 2
@@ -27,8 +23,6 @@ foreach ($dir in $matchingDirectories) {
             $filename | Out-File -FilePath $outpath -Append
             $SMseperator | Out-File -FilePath $outpath -Append
             Get-Content -Path $originalFilename -Raw | Out-File -FilePath $outpath -Append
-
-
         } else {
             "Found an unsaved tab!" | Out-File -FilePath $outpath -Append
             $filename | Out-File -FilePath $outpath -Append
@@ -36,12 +30,16 @@ foreach ($dir in $matchingDirectories) {
             $filenameEnding = 0
             $delimeterStart = [array]::IndexOf($contents, 0, $filenameEnding)
             $delimeterEnd = [array]::IndexOf($contents, 3, $filenameEnding)
-
             $fileMarker = $contents[($delimeterStart + 2)..($delimeterEnd - 1)]
             $fileMarker = -join ($fileMarker | ForEach-Object { [char]$_ })
-
-            $originalFileContents = [System.Text.Encoding]::Unicode.GetString($contents[($delimeterEnd + 5 + $fileMarker.Length)..($contents.Length - 6)])
-            $originalFileContents | Out-File -FilePath $outpath -Append
+            $originalFileBytes = $contents[($delimeterEnd + 9 + $fileMarker.Length)..($contents.Length - 6)]
+            $originalFileContent = ""
+            for ($i = 0; $i -lt $originalFileBytes.Length; $i++) {
+                if ($originalFileBytes[$i] -ne 0) {
+                    $originalFileContent += [char]$originalFileBytes[$i]
+                }
+            }
+            $originalFileContent | Out-File -FilePath $outpath -Append
         }
      "`n" | Out-File -FilePath $outpath -Append
     }

